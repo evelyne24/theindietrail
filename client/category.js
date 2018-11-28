4879530cfe32234a69b33df194e77c712dc36951
Evelina Vrabie <evelina@zinc-mission2.com>
2018-11-28 19:35:42 +0000

    Fixed Maps API key

--- client/category.js
+++ client/category.js
@@ -24,47 +24,45 @@ import qs from "qs";
 import "./scripts/front";
 import createCategoryMap from "./scripts/map-multi.js";
 
-$(function() {
-  const place = qs.parse(window.location.search, { ignoreQueryPrefix: true })
-    .place;
+const place = qs.parse(window.location.search, { ignoreQueryPrefix: true })
+  .place;
 
-  const itemTemplate = require("./templates/partials/dynamic/category-list-item.hbs");
+const itemTemplate = require("./templates/partials/dynamic/category-list-item.hbs");
 
-  $.getJSON("./json/manchester.json")
-    .done(function(json) {
-      const jsonItems = json.items;
-      $(".container h1").text(json.title);
-      $(".container p.lead").text(json.description);
+$.getJSON("./json/manchester.json")
+  .done(function(json) {
+    const jsonItems = json.items;
+    $(".container h1").text(json.title);
+    $(".container p.lead").text(json.description);
 
-      jsonItems.forEach((jsonItem, i) => {
-        const item = itemTemplate({
-          badge: jsonItem.badge,
-          name: jsonItem.name,
-          address: jsonItem.address,
-          description: jsonItem.description,
-          photo: jsonItem.photo
-        });
-        $("#items").append(item);
+    jsonItems.forEach((jsonItem, i) => {
+      const item = itemTemplate({
+        badge: jsonItem.badge,
+        name: jsonItem.name,
+        address: jsonItem.address,
+        description: jsonItem.description,
+        photo: jsonItem.photo
       });
-    })
-    .fail(function(jqxhr, textStatus, error) {
-      console.log(error);
+      $("#items").append(item);
     });
+  })
+  .fail(function(jqxhr, textStatus, error) {
+    console.log(error);
+  });
 
-  // coordinates for the center of the map
-  var lat = 40.732346;
-  var long = -74.0014247;
-  // json file path with the markers to display on the map
-  var jsonFile = "./json/addresses/manchester.json";
-  // if using with other than default style, change the path to the colour variant
-  // of the marker. E.g. to img/map-marker-violet.png.
-  var markerImage = "img/map-marker-default.png";
+// coordinates for the center of the map
+var lat = 53.5072024;
+var long = -2.281417;
+// json file path with the markers to display on the map
+var jsonFile = "./json/addresses/manchester.json";
+// if using with other than default style, change the path to the colour variant
+// of the marker. E.g. to img/map-marker-violet.png.
+var markerImage = "img/map-marker-default.png";
 
-  $.getJSON(jsonFile)
-    .done(function(json) {
-      const map = createCategoryMap(lat, long, json, markerImage);
-    })
-    .fail(function(jqxhr, textStatus, error) {
-      console.log(error);
-    });
-});
+$.getJSON(jsonFile)
+  .done(function(json) {
+    const map = createCategoryMap(lat, long, json, markerImage);
+  })
+  .fail(function(jqxhr, textStatus, error) {
+    console.log(error);
+  });
